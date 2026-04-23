e88ecac3f1c97381cb88ac2377d134d4f9e71180
danhtruong <thisisdanh@gmail.com>
2026-04-23 19:21:07 +0000

    Deploying to gh-pages from @ danhtruong/SSaS@2d1d2404217470b4a35d3015c68f4a5bb1d812b1 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "danhtruong/SSaS.github.io",
+    "data-repo": "danhtruong/SSaS",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
